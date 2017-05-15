ea6d64750485f35fcb7ffa91dc4b027ee0ca9034
Julio Nobrega Netto <julionn@yahoo-inc.com>
2017-05-15 09:47:58 -0700

    Solved Day Three Part One

---
+++ three/part_one.go
@@ -0,0 +1,36 @@
+package three
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func IsTriangle(triangles string) bool {
+	sidesAsString := strings.Split(triangles, " ")
+	sidesAsInts := make([]int, 0)
+
+	for _, side := range sidesAsString {
+		if side != "" {
+			sideAsInt, _ := strconv.Atoi(side)
+			sidesAsInts = append(sidesAsInts, sideAsInt)
+		}
+	}
+	sort.Ints(sidesAsInts)
+
+	return (sidesAsInts[0] + sidesAsInts[1]) > sidesAsInts[2]
+}
+
+func DayThreePartOne(input string) int {
+	lines := strings.Split(input, "\n")
+	trianglesQty := 0
+
+	for _, line := range lines {
+		cleanLine := strings.Trim(line, " ")
+		if IsTriangle(cleanLine) {
+			trianglesQty = trianglesQty + 1
+		}
+	}
+
+	return trianglesQty
+}
