fade6979d90fb183b0d6738ef5ce976647d6956e
Julio Nobrega Netto <julionn@yahoo-inc.com>
2017-05-14 20:41:46 -0700

    Solved Day Two

--- two/two.go
+++ two/two.go
@@ -2,31 +2,33 @@ package two
 
 import (
 	"math"
+	"strconv"
 	"strings"
 )
 
-func DayTwo(directions string) [4]float64 {
-	var code [4]float64
-
+func DayTwo(directions string) string {
 	lines := strings.Split(directions, "\n")
-	currentIndex := float64(5)
+	codeSize := len(lines)
+	code := make([]string, codeSize)
+
+	currentIndex := 5
 	for i, line := range lines {
 		lineDirections := strings.Split(line, "")
 		for _, direction := range lineDirections {
 			if direction == "U" && currentIndex > 3 {
 				currentIndex = currentIndex - 3
 			}
-			if direction == "R" && math.Mod(currentIndex, 3) != 0 {
+			if direction == "R" && math.Mod(float64(currentIndex), 3) != 0 {
 				currentIndex = currentIndex + 1
 			}
 			if direction == "D" && currentIndex < 7 {
 				currentIndex = currentIndex + 3
 			}
-			if direction == "L" && math.Mod(currentIndex, 3) != 1 {
+			if direction == "L" && math.Mod(float64(currentIndex), 3) != 1 {
 				currentIndex = currentIndex - 1
 			}
 		}
-		code[i] = currentIndex
+		code[i] = strconv.Itoa(currentIndex)
 	}
-	return code
+	return strings.Join(code, "")
 }
